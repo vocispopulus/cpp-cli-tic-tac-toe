de478b13a79447004f0bf90a5b647dde36689650
vocispopulus <vocispopulus@users.noreply.github.com>
2019-11-13 23:54:04 +0000

    Draw the board in one cout call, add a first turn

--- main.cpp
+++ main.cpp
@@ -4,16 +4,27 @@
 
 void draw_board(const std::array<int, 9>& arr)
 {
-  std::cout << arr[0] << "|" << arr[1] << "|" << arr[2] << "\n";
-  std::cout << "-----\n";
-  std::cout << arr[3] << "|" << arr[4] << "|" << arr[5] << "\n";
-  std::cout << "-----\n";
-  std::cout << arr[6] << "|" << arr[7] << "|" << arr[8] << "\n";
+  std::cout << arr[0] << "|" << arr[1] << "|" << arr[2] << std::endl
+            << "-----" << std::endl
+            << arr[3] << "|" << arr[4] << "|" << arr[5] << std::endl
+            << "-----" << std::endl
+            << arr[6] << "|" << arr[7] << "|" << arr[8] << std::endl;
+}
+
+int turn()
+{
+  int position;
+  std::cout << "Turn of player " << std::endl
+            << "Enter position desired: ";
+  std::cin >> position;
+  return position;
 }
 
 int main()
 {
   std::array<int, 9> squares = {0,0,0,0,0,0,0,0,0};
+  draw_board(squares);
+  squares[turn()] = 1;
   draw_board(squares);
   return 0;
 }
