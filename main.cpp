f47f2e56e22a77fcf5e47bfe40d00daed7e27f10
vocispopulus <vocispopulus@users.noreply.github.com>
2019-11-14 21:32:42 +0000

    Add turn loop and game ending conditions

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 
 #include <array>
 
+bool won(const std::array<int, 9>& arr)
+{
+  return false;
+}
+
+bool full(const std::array<int, 9>& arr)
+{
+  return false;
+}
+
 void draw_board(const std::array<int, 9>& arr)
 {
   std::cout << arr[0] << "|" << arr[1] << "|" << arr[2] << std::endl
@@ -11,20 +21,31 @@ void draw_board(const std::array<int, 9>& arr)
             << arr[6] << "|" << arr[7] << "|" << arr[8] << std::endl;
 }
 
-int turn()
+int enter_position()
 {
   int position;
   std::cout << "Turn of player " << std::endl
             << "Enter position desired: ";
   std::cin >> position;
+
   return position;
+}
+
+int switch_player(int current_player)
+{
+  if (current_player == 1) return 2;
+  else return 1;
 }
 
 int main()
 {
   std::array<int, 9> squares = {0,0,0,0,0,0,0,0,0};
-  draw_board(squares);
-  squares[turn()] = 1;
+  int player = 1;
+  while (!won(squares) || full(squares)) {
+    draw_board(squares);
+    squares[enter_position()] = player;
+    player=switch_player(player);
+  }
   draw_board(squares);
   return 0;
 }
